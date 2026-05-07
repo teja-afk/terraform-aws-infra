71fb01d50720603fc1c45076baa61282768bc1a3
teja-afk <tejapoosa123@gmail.com>
2026-05-07 23:23:07 +0530

    added security group in main.tf

--- main.tf
+++ main.tf
@@ -47,3 +47,67 @@ resource aws_route_table_association public_assoc {
 	route_table_id = aws_route_table.public_rt.id
 }
 
+# 6. Security Group
+resource aws_security_group terraweek_sg {
+	name = "terraweek-sg"
+	description = "Allow SSH and HTTP"
+	vpc_id = aws_vpc.terraweek_vpc.id
+
+	# SSH Access
+	ingress {
+		description= "SSH"
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	
+	# HTTP Acess
+	ingress {
+		description = "HTTP"
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	# Outbound Traffic
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	
+	tags = {
+		Name = "TerraWeek-SG"
+	}
+}
+
+# 7. EC2 Instance
+resource aws_instance terraweek_server {
+	ami = "ami-06c77cb49ac92a541"
+	instance_type = "t2.micro"
+	subnet_id = aws_subnet.public_subnet.id
+	vpc_security_group_ids = [aws_security_group.terraweek_sg.id]
+
+	lifecycle {
+    		create_before_destroy = true
+  	}
+	associate_public_ip_address = true
+	tags = {
+		Name = "TerraWeek-Server"
+	}
+}
+
+# S3 Bucket for Application Logs
+resource aws_s3_bucket app_logs {
+	bucket = "terraweek-app-logs-unique-12345"
+	depends_on = [aws_instance.terraweek_server]
+	
+	tags = {
+		Name = "TerraWeek-App-Logs"
+	}
+}
+
+
